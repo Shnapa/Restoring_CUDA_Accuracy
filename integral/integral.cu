72cf9e9407ca779154c3b7cc606521fab112d93f
YulianaHrynda <hrynda.pn@ucu.edu.ua>
2025-02-25 12:52:11 +0000

    Implemented integral on cuda

--- integral/integral.cu
+++ integral/integral.cu
@@ -1,52 +1,59 @@
-#include <cmath.h>
+#include <cmath>
 #include <vector>
+#include <iostream>
+#include <cuda_runtime.h>
+#define BLOCK_SIZE 16
+__device__ double func(double x, double y) {
+    double sums = 0.0;
 
-__device__ func(double x, double y) {
-    double sum1 = 0, sum2 = 0;
-
-    for (int i = 1; i < 5; i++) {
-        sum1 += i * cos((i + 1) * x + 1);
-        sum2 += i * cos((i + 1) * y + 1);
+    for (int i = -2; i <= 2; i++) {
+        for (int j = -2; j <= 2; j++){
+            sums += 1.0 / (5 * (i+2) + j + 3 + pow( x - 16*j , 6) + pow( y - 16*i , 6));
+        }
     }
-
-    return - sum1 * sum2;
+    return 1.0 / (0.002 + sums); 
 }
 
-__global__ double riemannSum(double x1, double x2,
+__global__ void riemannSum(double x1, double x2,
                              double y1, double y2,
-                             int max_interations,
-//                             int steps_x, int steps_y,
-//                             double abs_err, double rel_err,
+                             int max_iter,
                              double *result) {
 
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
 
-    int dx, dy = abs(x1 - x2) / max_interations, abs(y1 - y2) / max_interations;
+    double dx = (x2 - x1) / max_iter; 
+    double dy = (y2 - y1) / max_iter;
 
-    if (i < n && j < n) {
-        double x = x1 + (i + 0.5) * dx;
-        double y = y1 + (j + 0.5) * dy;
+    if (i < max_iter && j < max_iter) {
+        double x = x1 + i * dx; 
+        double y = y1 + j * dy;
         double f_val = func(x, y) * dx * dy;
 
         atomicAdd(result, f_val);
+    }
 }
 
-int main(){
+double riemman_return(double x1, double x2, double y1, double y2, int max_iter){
     double h_result = 0.0, *d_result;
     cudaMalloc((void**)&d_result, sizeof(double));
-
-    const int BLOCK_SIZE = 16;
-    dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
-    dim3 gridSize((n + BLOCK_SIZE - 1) / BLOCK_SIZE, (n + BLOCK_SIZE - 1) / BLOCK_SIZE);
-
     cudaMemcpy(d_result, &h_result, sizeof(double), cudaMemcpyHostToDevice);
+    
+    dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 gridSize((max_iter + BLOCK_SIZE - 1) / BLOCK_SIZE, (max_iter + BLOCK_SIZE - 1) / BLOCK_SIZE);
 
-    riemannSum<<<gridSize, BLOCK_SIZE>>>(-100, 100, -100, 100, 30, d_result);
+    riemannSum<<<gridSize, blockSize>>>(x1, x2, y1, y2, max_iter, d_result);
 
     cudaMemcpy(&h_result, d_result, sizeof(double), cudaMemcpyDeviceToHost);
 
     cudaFree(d_result);
 
-    printf("%lf\n", h_result);
+    return h_result;
 }
+
+int main(){
+	int n = 10000;
+	double result = riemman_return(-50, 50, -50, 50, n);
+	std::cout << "Result: " << result << std::endl;
+}
+
