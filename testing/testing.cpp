5915ba2d0491191bcdbc494cafa66a2b90005a2e
Khrystya Korets <hkorets0411@gmail.com>
2025-04-14 23:51:48 +0300

    Update testing.cpp

--- testing/testing.cpp
+++ testing/testing.cpp
@@ -1,47 +1,71 @@
 //
-// Created by hko on 25.03.2025.
+// Created by Admin on 02.04.2025.
 //
 
 #include "testing.h"
-#include "matrix-multiplication/multBrut.h"
-using Matrix = std::vector<int>;
+#include <vector>
+#include <string>
+#include "src/matrixParser.cpp"
+#include "src/generateMatrices.cpp"
 
-std::tuple<Matrix, size_t, size_t> readMatrixFromFile(const std::string& filename) {
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        throw std::runtime_error("Failed to open file: " + filename);
+void generate_matrices(size_t m, size_t n, float* A) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dist(-1000.0, 1000.0);
+
+    size_t totalA = m * n;
+    for (size_t k = 0; k < totalA; ++k) {
+        A[k] = dist(gen);
     }
-
-    size_t m, n;
-    file >> m >> n;
-
-    Matrix C(m * n);
-    for (size_t i = 0; i < m * n; ++i) {
-        file >> C[i];
-    }
-
-    return {C, m, n};
 }
 
-bool compareMatrices(const Matrix& A, size_t m1, size_t n1,
-                     const Matrix& B, size_t m2, size_t n2,
-                     const std::tuple<Matrix, size_t, size_t>& C_tuple) {
-    const auto& [C, m3, n3] = C_tuple;
-
-    if (m3 != m1 || n3 != n2) {
-        return false;
-    }
-
-    Matrix correctMul = multiplication(A, m1, n1, B, m2, n2);
-
-    if (C.size() != correctMul.size()) {
-        return false;
-    }
-
-    for (size_t i = 0; i < C.size(); ++i) {
-        if (C[i] != correctMul[i]) {
-            return false;
+bool compare_matrices(float* A, float* B, size_t m, size_t n, float epsilon) {
+    for (size_t i = 0; i < m; ++i) {
+        for (size_t j = 0; j < n; ++j) {
+            if (std::fabs(A[i * n + j] - B[i * n + j]) > epsilon) {
+                return false; // Якщо різниця більша за epsilon
+            }
         }
     }
     return true;
 }
+
+
+
+void write_matrices_to_file(const float* matrix1, const float* matrix2, size_t m, size_t n, size_t p, const std::string& filename) {
+    std::ofstream outfile(filename);
+    if (!outfile.is_open()) {
+        std::cerr << "Error opening file: " << filename << std::endl;
+        return;
+    }
+
+    for (size_t i = 0; i < m; ++i) {
+        for (size_t j = 0; j < n; ++j) {
+            outfile << matrix1[i * n + j];
+            if (j < n - 1) {
+                outfile << " ";
+            }
+        }
+        if (i < m - 1) {
+            outfile << " ";
+        }
+    }
+
+    outfile << "\n";
+
+    for (size_t i = 0; i < n; ++i) {
+        for (size_t j = 0; j < p; ++j) {
+            outfile << matrix2[i * p + j];
+            if (j < p - 1) {
+                outfile << " ";
+            }
+        }
+        if (i < n - 1) {
+            outfile << " ";
+        }
+    }
+
+    outfile.close();
+}
+
+
