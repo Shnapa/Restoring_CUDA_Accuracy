98886a7ed1904f80f5f22f977fdea8840a5eb256
Khrystya Korets <hkorets0411@gmail.com>
2025-04-15 19:32:35 +0300

    Update cudaMul.cu

--- src/cudaMul.cu
+++ src/cudaMul.cu
@@ -16,7 +16,9 @@ __global__ void cudaMul(const float* A, const float* B, float* C, size_t m, size
         C[row * k + col] = sum;
     }
 }
-
+inline bool compareFloats(float a, float b, float epsilon = 1e-5f) {
+    return fabs(a - b) < epsilon;
+}
 void compare(const float* h_C, size_t m, size_t n, size_t k, const std::string& filePath) {
     size_t A_elements = m * n;
     size_t B_elements = n * k;
